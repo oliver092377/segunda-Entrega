9f2c933468135a1547558d6d35f7549b8887253d
alumno <omayta@unsa.edu.pe>
2021-12-14 17:38:38 +0000

    segundo commit

---
+++ 2daEntrega/delete.pl
@@ -0,0 +1,86 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use CGI;
+use DBI;
+
+
+my $q = CGI->new;
+print $q->header('text/xml;charset=UTF-8');
+
+my $usuario = $q->param("owner");
+my $titulo = $q->param("title");
+
+if(defined($usuario) and defined($titulo)){
+  if(checkLogin($usuario, $titulo)){
+    eliminar();
+    mostrar();
+  }else{
+    showLogin('Usuario o contraseña equivocados, vuela a intentarlo');
+  }
+}else{
+  print "sdsf";
+  showLogin();
+}
+sub checkLogin{
+  my $userQuery = $_[0];
+  my $title = $_[1];
+
+  my $user = 'alumno';
+  my $password = 'pweb1';
+  my $dsn = 'DBI:MariaDB:database=pweb1;host=localhost';
+  my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
+
+  my $sql = "SELECT * FROM Articles WHERE owner=? AND title=?";
+  my $sth = $dbh->prepare($sql);
+  $sth->execute($userQuery, $title);
+  my @row = $sth->fetchrow_array;
+  $sth->finish;
+  $dbh->disconnect;
+  return @row;
+}
+sub eliminar{
+  my $user = 'alumno';
+  my $password = 'pweb1';
+  my $dsn = 'DBI:MariaDB:database=pweb1;host=localhost';
+  my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
+
+  my $sql = "DELETE FROM Articles WHERE owner='$usuario' AND title='$titulo'";
+  my $sth = $dbh->prepare($sql);
+  $sth->execute();
+  $sth->finish;
+  $dbh->disconnect;
+}
+sub retornaEsto{
+  my $quiero = $_[0];
+  my $deTabla = $_[1];
+
+  my $user = 'alumno';
+  my $password = 'pweb1';
+  my $dsn = 'DBI:MariaDB:database=pweb1;host=localhost';
+  my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
+
+  my $sql = "SELECT $quiero FROM $deTabla WHERE userName=?";
+  my $sth = $dbh->prepare($sql);
+  $sth->execute($usuario);
+  my @row = $sth->fetchrow_array;
+  $sth->finish;
+  $dbh->disconnect;
+  return $row[0];
+}
+sub mostrar{
+  print <<"HTML";
+    <?xml version="1.0" encoding="uft-8">
+    <article>
+      <owner>$usuario</owner>
+      <title>$titulo</title>
+    </article>
+HTML
+}
+sub showLogin{
+  print <<"HTML";
+    <?xml version="1.0" encoding="uft-8">
+    <article>
+    </article>
+HTML
+}
